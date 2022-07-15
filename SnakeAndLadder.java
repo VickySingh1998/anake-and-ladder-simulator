b95ff9239d423ee30feccc9fa7f57d8106a2168e
vickysingh1998 <vickysingh8697@gmail.com>
2022-07-15 23:09:06 +0530

    added: start position of player

---
+++ SnakeAndLadder.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        int player1Position = 0;
+        System.out.println("Game begins: player1 at position: " + player1Position);
+    }
+}
