07f9b982d53537fafc69953f0a20299a357d3f77
vickysingh1998 <vickysingh8697@gmail.com>
2022-07-15 23:29:55 +0530

    modified: to check for options

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,9 +1,33 @@
 package com.bridgelabz;
 
+import java.util.*;
 public class SnakeAndLadder {
+
+    static final int NO_PLAY = 0;
+    static final int LADDER = 1;
+    static final int SNAKE = 2;
     public static void main(String[] args) {
         int position = 0;
-        int diceRoll = (int) Math.floor(Math.random()*10) % 6 + 1;
-        System.out.println("Player rolled the die and got "+diceRoll);
+        Random ran = new Random();
+        int dice = ran.nextInt(6)+1;
+        System.out.println("dice: "+dice);
+        int optionCheck = ran.nextInt(3);
+        System.out.println("Option Check : "+optionCheck);
+
+        if (optionCheck == LADDER) {
+            System.out.println("Ladder");
+            position = position + dice;
+        }
+        else if (optionCheck == SNAKE)
+        {
+            System.out.println("Snake");
+            position = position - dice;
+        }
+        else {
+            System.out.println("No Play");
+        }
+        if (position<0)
+            position=0;
+        System.out.println("new position: "+position);
     }
 }
