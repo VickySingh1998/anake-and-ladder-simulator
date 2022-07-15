1e9cf16aefee8608fcad01a8873c0b9d2d997652
vickysingh1998 <vickysingh8697@gmail.com>
2022-07-15 23:39:36 +0530

    added: new feature to repeat till the player wins

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -8,26 +8,29 @@ public class SnakeAndLadder {
     static final int SNAKE = 2;
     public static void main(String[] args) {
         int position = 0;
-        Random ran = new Random();
-        int dice = ran.nextInt(6)+1;
-        System.out.println("dice: "+dice);
-        int optionCheck = ran.nextInt(3);
-        System.out.println("Option Check : "+optionCheck);
 
-        if (optionCheck == LADDER) {
-            System.out.println("Ladder");
-            position = position + dice;
+        while (position < 100) {
+
+            Random ran = new Random();
+            int dice = ran.nextInt(6) + 1;
+            System.out.println("dice: " + dice);
+            int optionCheck = ran.nextInt(3);
+            System.out.println("Option Check : " + optionCheck);
+
+            if ((optionCheck == LADDER) && (position + dice) <=100) {
+                System.out.println("Ladder");
+                position = position + dice;
+            } else if (optionCheck == SNAKE) {
+                System.out.println("Snake");
+                position = position - dice;
+            } else {
+                System.out.println("No Play");
+            }
+            if (position < 0)
+                position = 0;
+            System.out.println("new position: " + position);
         }
-        else if (optionCheck == SNAKE)
-        {
-            System.out.println("Snake");
-            position = position - dice;
-        }
-        else {
-            System.out.println("No Play");
-        }
-        if (position<0)
-            position=0;
-        System.out.println("new position: "+position);
+        System.out.println("");
+        System.out.println(" **You Won** ");
     }
 }
